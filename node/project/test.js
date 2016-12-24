cac8a3b49bf6b6ac0d729288b69eb81078d05a9d
LobbyXX <30781387@qq.com>
2016-12-24 12:08:12 +0800

    c

--- node/project/test.js
+++ node/project/test.js
@@ -1,20 +1,27 @@
 // console.log("hello world!");
 
-var http = require("http");
+var express = require("express");
 // var configs = require("./configs/server.json");
 
-console.log(process.env);
+// console.log(process.env);
 
-var server = http.createServer(function (request, response) {
+var app = express();
 
-	// 发送 HTTP 头部
-	// HTTP 状态值: 200 : OK
-	// 内容类型: text/plain
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+app.use(express.static("html"));
 
-	// 发送响应数据 "Hello World"
-	response.end('Hello World\n');
-}).listen(8088);
+var server = app.listen(8088, function(){
+	console.log(`Server running at ${server.address().address}:${server.address().address}`);
+});
+
+// var server = http.createServer(function (request, response) {
+//
+// 	// 发送 HTTP 头部
+// 	// HTTP 状态值: 200 : OK
+// 	// 内容类型: text/plain
+// 	response.writeHead(200, {'Content-Type': 'text/plain'});
+//
+// 	// 发送响应数据 "Hello World"
+// 	response.end('Hello World\n');
+// }).listen(8088);
 
 // 终端打印如下信息
-console.log(`Server running at ${server.address}:8088`);
